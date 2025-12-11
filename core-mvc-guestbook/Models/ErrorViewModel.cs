77fd6b505bd6996f03019a7dd9e270ebbc7087ce
Todd Criswell <TCriswell@ameritfleet.com>
2025-12-11 09:45:16 -0500

    Add project files.

---
+++ core-mvc-guestbook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace core_mvc_guestbook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
